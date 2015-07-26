66a35200d3b4a2e2a1af564608f23d4d4f0bcfa8
tjz <415800467@qq.com>
2015-07-26 18:18:19 +0800

    修改1.0

--- js/main.js
+++ js/main.js
@@ -1,49 +1,9 @@
-
-var myStyles = {},
-    myTabs = [];
-    //alert(1)
-$.get('/myStyle.json', function(resObj) {
-    var resObj = JSON.parse(resObj);
-    for (var key in resObj) {
-        myStyles[key.toLowerCase()] = resObj[key];
-        myTabs.push(key.toLowerCase());
-    };
-});
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-    var app = {
-        initialize: function() {
-            var self = this;
-            this.myStyles = myStyles;
-            this.myTabs = myTabs;
-            this.updateCss();
-        },
-        updateCss: function(){
-
-            var tabUrl = tab.url.toLowerCase(),
-                tabTitle = tab.title,
-                cssCode = this.myStyles["default"];
-            if(this.myStyles[tabUrl]){
-                cssCode += this.myStyles[tabUrl];
-            } else {
-                for (var i = 0, len = this.myTabs.length; i < len; i++) {
-                    if(tabUrl.indexOf(this.myTabs[i]) > -1) {
-                        tabUrl = this.myTabs[i];
-                        cssCode += this.myStyles[tabUrl];
-                        break;
-                    }
-                }
-            }
-
-            chrome.browserAction.setIcon({
-                path: 'icon-open.png',
-                tabId: tab.id
-            })
-            chrome.tabs.insertCSS(tab.id,{
-                code: cssCode
-            });
-
-        }
-    }
-    app.initialize();
-});
+// chrome.webNavigation.onDOMContentLoaded.addListener(function(tab) {
+//     alert(tab)
+//     tabId = tab.id;
+//     alert(tabId)
+//     updataCSS();
+// })
+//window.tab = 'test';
+//var tab = chrome.tabs.getCurrent();
+//alert(tab);
